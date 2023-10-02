8fb5898ca6d42586e327fd64a5ce843c1f32b03b
Metin Karagöz <metinkzv@gmail.com>
2023-10-02 20:52:04 +0300

    finish project and include experiences in readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rock-paper-scissors
+
+- There was no need for computer results in startRound function. Else would do just fine.
+
+- It is best if I would have declared a variable for startRound function inside game function.
+  Since we will be taking the result of playRound 5 times, it should be declared inside for loop.
+
+- I have overcomplexed the game even tho it is simple. Declarations such as roundWinner etc. are not necessary.
--- script.js
+++ script.js
@@ -3,30 +3,50 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let roundWinner = "";
-
 function startRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
   if (playerSelection === computerSelection) {
-    roundWinner = "tie";
     return "It's a tie.";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    roundWinner = "player";
     return "Player wins!";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
-  ) {
-    roundWinner = "computer";
+  } else {
     return "Computer wins!";
   }
 }
 
-let playerSelection = "paper";
-let computerSelection = getComputerChoice();
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-console.log(startRound(playerSelection, computerSelection));
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt(
+      "What is your choice? (rock/paper/scissors)"
+    );
+    const computerSelection = getComputerChoice();
+    const result = startRound(playerSelection, computerSelection);
+
+    if (result === "Computer wins!") {
+      computerScore++;
+    } else if (result === "Player wins!") {
+      playerScore++;
+    }
+  }
+
+  console.log(
+    `Final score | Computer: ${computerScore}, Player: ${playerScore}.`
+  );
+
+  if (playerScore > computerScore) {
+    console.log("Player wins the game!");
+  } else if (playerScore < computerScore) {
+    console.log("Computer wins the game!");
+  }
+}
+
+game();
